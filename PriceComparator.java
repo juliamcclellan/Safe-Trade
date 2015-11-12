7b906d706de842c2be65c40708b688b633991c21
giacalone99 <Luke@porrazzo.campus.middlesex.edu>
2015-11-12 13:03:54 -0500

    Finished the class
    As it turns out I forgot to finish the compare method, so I did that.

--- PriceComparator.java
+++ PriceComparator.java
@@ -31,14 +31,23 @@ public class PriceComparator implements Comparator<TradeOrder> {
 	 * 0 if both orders are market orders;
 	 * -1 if order1 is market and order2 is limit;
 	 * 1 if order1 is limit and order2 is market;
+	 * the difference in prices, rounded to the nearest cent, if both order1 and order2 are limit orders. 
+	 * In the latter case, the difference returned is cents1 - cents2 or cents2 - cents1, depending on whether 
+	 * this is an ascending or descending comparator (ascending is true or false).
 	 */
 	public int compare(TradeOrder order1, TradeOrder order2) {
 		if(order1.isMarket() && order2.isMarket())
 			return 0;
 		else if(order1.isMarket() && !order2.isMarket())
 			return -1;
-		else
+		else if(!order1.isMarket() && order2.isMarket())
 			return 1;
+		else { //returns the difference in CENTS
+			if(ascending) //if ascending mode
+				return (int) ((order1.getPrice() * 100) - (order2.getPrice() * 100));
+			else //if descending mode
+				return (int) ((order2.getPrice() * 100) - (order1.getPrice() * 100));
+		}
 	}
 	
 }
